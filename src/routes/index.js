a96b63325a860aefa0bfab85d96fc7074329a902
hedhogeg <hedhogeg@yonsei.ac.kr>
2022-01-25 01:22:14 +0900

    feat: draft server

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('main');
+});
+
+router.get('/game', function(req, res, next) {
+  res.render('game');
+});
+
+module.exports = router;
