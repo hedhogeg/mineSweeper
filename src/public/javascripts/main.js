d2ba8009e02f00c310bedf80e738b05597afff7b
hedhogeg <hedhogeg@yonsei.ac.kr>
2022-01-26 04:10:17 +0900

    fix: set custom limit

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,6 +1,7 @@
 let row_num = 0
 let col_num = 0
 let mine_num = 0
+let mine_max = 0
 let custom = false
 let selected = false
 
@@ -26,12 +27,15 @@ const setGame = (row, col, mine) => {
 const select = (dif) => {
     const doc_custom = document.getElementById("custom_box")
     if (dif == 'easy') {
+        custom = false
         setGame(9, 9, 10)
         doc_custom.setAttribute("style", "visibility: hidden;")
     } else if (dif == 'normal') {
+        custom = false
         setGame(16, 16, 40)
         doc_custom.setAttribute("style", "visibility: hidden;")
     } else if (dif == 'hard') {
+        custom = false
         setGame(16, 30, 99)
         doc_custom.setAttribute("style", "visibility: hidden;")
     } else if (dif == 'custom') {
@@ -41,35 +45,53 @@ const select = (dif) => {
     setSelected(dif)
 }
 
+const setMaxMine = () => {
+    // max number of mine
+    row_num = document.getElementById("row").value
+    col_num = document.getElementById("col").value
+    mine_max = Math.floor(row_num * col_num / 4.5)
+    console.log(mine_max)
+}
+
 const gameStart = () => {
     if (!selected) {
         alert("Select difficulty")
     } else {
+        let error = false
         const form = document.getElementById("custom_box")
         if (custom) {
+            row_num = document.getElementById("row").value
+            col_num = document.getElementById("col").value
+            mine_num = document.getElementById("mine").value
             if (!row_num || !col_num || !mine_num) {
                 alert("If you want custom game, enter all")
+                error = true
+            } else if (row_num < 9 || row_num > 30) {
+                alert("Rows can be 9 to 30")
+                error = true
+            } else if (col_num < 9 || col_num > 30) {
+                alert("Columns can be 9 to 30")
+                error = true
+            } else if (mine_num < 10 || mine_num > mine_max) {
+                alert(`Mines can be 10 to ${mine_max}`)
+                error = true
             }
         }
         
-        form.submit()
+        if (error) {
+            location.reload()
+        } else {
+            form.submit()
+        }
     }
     
 }
 
-// const setMaxMine = () => {
-    // max number of mine
-//     row_num = document.getElementById("row").value
-//     col_num = document.getElementById("col").value
-//     const mine_max = Math.floor(row_num * col_num / 4.5)
-//     const doc_mine = document.getElementById("mine")
-// }
+const init = () => {
+    const doc_row = document.getElementById("row")
+    const doc_col = document.getElementById("col")
+    doc_row.addEventListener("change", setMaxMine)
+    doc_col.addEventListener("change", setMaxMine)
+}
 
-// const init = () => {
-//     const doc_row = document.getElementById("row")
-//     const doc_col = document.getElementById("col")
-//     doc_row.addEventListener("change", setMaxMine)
-//     doc_col.addEventListener("change", setMaxMine)
-// }
-
-// init()
+init()
